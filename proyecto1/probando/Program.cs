b2c6c23841e57ac30b89f272170ee722b79264e7
kapireloaded <kapireloaded@gmail.com>
2026-03-04 02:04:40 -0600

    cadena nueva

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, kapireloaded!");
 
 string stripe_api_key="sk_test_51N8n9sKj2mLh7uXo5q1Zt3v6z8y9";
+
+string testingConnection="Server=localhost;Database=miPrimerRepo;User Id=sa;Password=your_password;";       
